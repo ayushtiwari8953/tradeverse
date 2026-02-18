6e5f67ab65ceda0d8524b630549680160bef8b1e
Delta student <someone@email.com>
2026-02-18 16:45:59 +0530

    fix broken submodule dashbord

---
+++ dashbord/src/components/BuyActionWindow.js
@@ -0,0 +1,98 @@
+import React, { useContext,useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+import GeneralContext from "./GeneralContext";
+
+import "./BuyActionWindow.css";
+
+const BuyActionWindow = ({ uid }) => {
+  const [stockQuantity, setStockQuantity] = useState(1);
+  const [stockPrice, setStockPrice] = useState(0.0);
+
+  const { closeBuyWindow } = useContext(GeneralContext);
+  // const { closeSellWindow } = useContext(GeneralContext);
+
+  const handleBuyClick = async () => {
+    await axios.post("http://localhost:3002/newOrder", {
+      name: uid,
+      qty: stockQuantity,
+      price: stockPrice,
+      mode: "BUY",
+    });
+
+    closeBuyWindow();
+  };
+
+//   const placeOrder = async (mode) => {
+ 
+//      await axios.post("http://localhost:3002/newOrder", {
+//       name: uid,
+//       qty: Number(stockQuantity),
+//       price: Number(stockPrice),
+//       mode: mode,
+//     });
+
+//     console.log("ORDER SUCCESS:", res.data);
+
+//     if (mode === "BUY") closeBuyWindow();
+//     if (mode === "SELL") closeSellWindow();
+
+//   } 
+// };
+
+
+  const handleCancelClick = () => {
+    closeBuyWindow();
+    // closeSellWindow();
+  };
+  
+
+  return (
+    <div className="container" id="buy-window" draggable="true">
+      <div className="regular-order">
+        <div className="inputs">
+          <fieldset>
+            <legend>Qty.</legend>
+            <input
+              type="number"
+              name="qty"
+              id="qty"
+              onChange={(e) => setStockQuantity(e.target.value)}
+              value={stockQuantity}
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Price</legend>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              step="0.05"
+              onChange={(e) => setStockPrice(e.target.value)}
+              value={stockPrice}
+            />
+          </fieldset>
+        </div>
+      </div>
+
+      <div className="buttons">
+        <span>Margin required ₹140.65</span>
+        <div>
+          <Link className="btn btn-blue" onClick={handleBuyClick}>
+            Buy
+          </Link>
+          {/* <Link className="btn btn-blue" onClick={placeOrder}>
+            Sell
+          </Link> */}
+          <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
+            Cancel
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BuyActionWindow;
---
+++ dashbord/src/components/SellActionWindow.js
@@ -0,0 +1,77 @@
+import React, { useContext,useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+import GeneralContext from "./GeneralContext";
+
+import "./SellActionWindow.css";
+
+const SellActionWindow = ({ uid }) => {
+  const [stockQuantity, setStockQuantity] = useState(1);
+  const [stockPrice, setStockPrice] = useState(0.0);
+
+const { closeSellWindow } = useContext(GeneralContext);
+
+  const handleSellClick = async () => {
+    console.log("🔥 SELL BUTTON CLICKED");
+    
+    await axios.post("http://localhost:3002/newOrder", {
+      name: uid,
+      qty: stockQuantity,
+      price: stockPrice,
+      mode: "SELL",
+    });
+
+    closeSellWindow();
+  };
+
+
+  const handleCancelClick = () => {
+    closeSellWindow();
+  };
+
+  return (
+    <div className="container" id="sell-window" draggable="true">
+      <div className="regular-order">
+        <div className="inputs">
+          <fieldset>
+            <legend>Qty.</legend>
+            <input
+              type="number"
+              name="qty"
+              id="qty"
+              onChange={(e) => setStockQuantity(e.target.value)}
+              value={stockQuantity}
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Price</legend>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              step="0.05"
+              onChange={(e) => setStockPrice(e.target.value)}
+              value={stockPrice}
+            />
+          </fieldset>
+        </div>
+      </div>
+
+      <div className="buttons">
+        <span>Margin required ₹140.65</span>
+        <div>
+          <button className="btn btn-blue" onClick={handleSellClick}>
+            Sell
+          </button>
+          <button to="" className="btn btn-grey" onClick={handleCancelClick}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SellActionWindow;
